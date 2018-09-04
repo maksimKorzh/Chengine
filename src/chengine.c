cff718a9a91b4504ca0f5d7675bc0e3088249a6b
Maksim Korzh <freesoft.for.people@gmail.com>
2018-09-04 19:15:03 +0300

    Added files via upload

--- src/chengine.c
+++ src/chengine.c
@@ -4,6 +4,11 @@
 int main()
 {
 	CHESSBOARD board[1];
+	/*ParseFen(board, "rnbqkbnr/pppp1ppp/8/4p3/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 ");
+	PrintBoard(board);
+	printf("%d\n", EvaluatePosition(board));*/
+	
+	
 	UciLoop(board);
 	
 	return 0;
